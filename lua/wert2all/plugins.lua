0fe1c06fd41b0777cca5f5df157ef9e346efe72c
wert2all <wert2all@gmail.com>
2023-12-20 21:29:23 +0200

    Add git signs

--- lua/wert2all/plugins.lua
+++ lua/wert2all/plugins.lua
@@ -97,6 +97,9 @@ return packer.startup(function(use)
   }
   use { 'nvim-neo-tree/neo-tree.nvim'  }
 
+  -- git signs
+  use {'lewis6991/gitsigns.nvim' }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
